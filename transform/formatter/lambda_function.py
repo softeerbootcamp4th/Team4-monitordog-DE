032aec16bcb6c1198d881234315b7326c0557296
steelbear <choisteelbear@gmail.com>
2024-08-25 16:46:11 +0900

    커뮤니티 지정자 수정 및 JSONDecodeEerror 임시조치

--- transform/formatter/lambda_function.py
+++ transform/formatter/lambda_function.py
@@ -15,56 +15,62 @@ logger.setLevel(logging.INFO)
 
 
 def preprocess(input_file_obj, output_file_obj, data_source, car_model):
+    '''
+    수집된 게시글이 담긴 S3 오브젝트에서 형식 통일 및 데이터 전처리 후 저장
+    '''
+    # Todo: 보배드림에서 클래스 이름이 변경됨에 따라 데이터를 수집하지 못한 경우도 발생
+    # 이에 제대로 저장되지 않은 데이터는 무시하고 온전히 기록된 데이터만 처리
+    # 크롤러에서 발생한 문제 수정 후 해당 JSONDecodeError 무시를 위한 try-except 문을 지울 것
     while True:
-        line = input_file_obj.readline()
+        try:
+            line = input_file_obj.readline()
+        except:
+            break
+        
         if not line:
             break
-        line = line.decode('utf-8')
 
-        post_info = json.loads(line)
+        try:
+            line = line.decode('utf-8')
+            post_info = json.loads(line)
+        except Exception as e:
+            logger.error(str(e))
+            continue
+        
         if len(post_info['created_at'].strip()) == 0:
             continue
 
-        post_info = preprocess_post(post_info, data_source)
-        post_info['data_source'] = data_source
-        post_info['car_model'] = car_model
+        try:
+            post_info = preprocess_post(post_info, data_source)
+            post_info['data_source'] = data_source
+            post_info['model'] = car_model
 
-        processed_line = (json.dumps(post_info, ensure_ascii=False) + '\n').encode('utf-8')
+            processed_line = (json.dumps(post_info, ensure_ascii=False) + '\n').encode('utf-8')
 
-        output_file_obj.write(processed_line)
-
-
-def folder2source(foldername):
-    if foldername == 'dc':
-        return 'dcinside'
-    elif foldername == 'naver':
-        return 'naver'
-    elif foldername == 'bobae':
-        return 'bobae'
-    elif foldername == 'clien':
-        return 'clien'
-    else:
-        raise ValueError(f"Cannot use folder name '{foldername}'")
+            output_file_obj.write(processed_line)
+        except Exception as e:
+            logger.error(str(e))
+            continue
 
 
 def lambda_handler(event, context):
     try:
+        # 이벤트 파싱
         s3_event = event['Records'][0]['s3']
         source_bucket = s3_event['bucket']['name']
-        # key: dc/dc_아이오닉6_2024-8-10_14-00-00.jsonl
+        # key: raw/2024-08-10T14-00-00/dc_아이오닉6_2024-08-10_14-00-00.jsonl
         source_key = urllib.parse.unquote_plus(s3_event['object']['key'])
-        source_basename = os.path.splitext(source_key)[0].split('/') 
-        source_folder = source_basename[0]
+        source_filename = source_key.split('/')[-1]
 
-        source_file = source_basename[1].split('_')
-        data_source = '_'.join(source_file[:-3])
-        car_model = source_file[-3]
-        source_date = 'T'.join(source_file[-2:])
+        # Source 정보
+        data_source, car_model, source_date = source_filename.split('_')
+        source_date = source_date.split('.')[0]
         
+        # Destination 정보
         destination_bucket = source_bucket  # 또는 다른 버킷을 지정할 수 있습니다.
-        destination_key = f"done/{source_date}/{data_source}_{car_model}_{source_date}{os.path.splitext(source_key)[1]}"
+        destination_key = f"formatted/{source_date}/{source_filename}"
 
-        logger.info(f"Processing {source_key} from bucket {source_bucket}")
+        logger.info("Processing %s from bucket %s", source_key, source_bucket)
 
         response = s3_client.get_object(Bucket=source_bucket, Key=source_key)
         input_file_obj = response['Body']
@@ -72,14 +78,13 @@ def lambda_handler(event, context):
         output_file_obj = BytesIO()
 
         # 파일을 한 줄씩 처리합니다
-        data_source = folder2source(source_folder)
         preprocess(input_file_obj, output_file_obj, data_source, car_model)
 
         output_file_obj.seek(0)
 
         s3_client.put_object(Bucket=destination_bucket, Key=destination_key, Body=output_file_obj.getvalue())
 
-        logger.info(f"Processed and saved as {destination_key} in bucket {destination_bucket}")
+        logger.info("Processed and saved as %s in bucket %s", destination_key, destination_bucket)
         
         return {
             'statusCode': 200,
@@ -89,7 +94,7 @@ def lambda_handler(event, context):
             })
         }
     except Exception as e:
-        logger.info(f'Error processing {source_bucket}/{source_key}: {str(e)}')
+        logger.info('Error processing %s/%s: %s', source_bucket, source_key, str(e))
         return {
             'statusCode': 500,
             'body': json.dumps(f'Error processing {source_bucket}/{source_key}: {str(e)}')

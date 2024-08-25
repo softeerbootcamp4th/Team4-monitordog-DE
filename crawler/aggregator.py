bdd5b38ec2660d04707075804d53f295675722bb
jang-namu <ironhee8005@gmail.com>
2024-08-26 03:43:11 +0900

    refactor: 메시지 수집기를 공통 컴포넌트로 재작성

--- crawler/aggregator.py
+++ crawler/aggregator.py
@@ -1,5 +1,9 @@
 import json
 import boto3
+import os 
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 sqs = boto3.client('sqs')
 s3 = boto3.client('s3')
@@ -23,13 +27,15 @@ def lambda_handler(event, context):
             break
         
         for message in response['Messages']:
-            messages.append(json.loads(message["Body"]))
-            # 메시지 삭제
-            sqs.delete_message(
-                QueueUrl=queue_url,
-                ReceiptHandle=message['ReceiptHandle']
-            )
-
+            try:
+                messages.append(json.loads(message["Body"]))
+                # 메시지 삭제
+                sqs.delete_message(
+                    QueueUrl=queue_url,
+                    ReceiptHandle=message['ReceiptHandle']
+                )
+            except Exception as e:
+                logger.info("에러원인: " + str(e))
     # 메시지가 없으면 작업 종료
     if not messages:
         return {'statusCode': 204, 'body': 'No messages to process'}
@@ -37,15 +43,27 @@ def lambda_handler(event, context):
     # 메시지를 JSONL 포맷으로 변환
     jsonl_data = "\n".join([json.dumps(msg, ensure_ascii=False) for msg in messages])
 
+
+    parsed_file_name = file_name.split("_")
+    source_date = parsed_file_name[-1]
+        
     # S3에 업로드
     s3.put_object(
         Body=jsonl_data, 
         Bucket=bucket_name, 
-        Key=f"{bucket_path}/{file_name}",
+        Key=f"raw/{source_date}/{file_name}.jsonl",
         ContentType='application/jsonl'
         )
+        
+    # 포맷팅 -> 감정부석 -> 키워드 결과 추가 후 폴링 루프에서 확인하기 위함
+    destination_key = f"keywords/{source_date}/{file_name}.jsonl"
+    
+    body = {
+        "bucket_name": bucket_name, 
+        "file_name": destination_key
+    }
     
     return {
         'statusCode': 200,
-        'body': f'Successfully uploaded {len(messages)} messages to {bucket_path}/{file_name} in {bucket_name}.'
+        'body': body
     }
